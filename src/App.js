86da334a6e8e7c9069e953dbe0f8a536626dcb11
Matty Witney <mattwitney@gmail.com>
2023-01-26 08:31:05 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import KekambasList from './components/KekambasList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App() {
+    
+    const [names, setNames] = useState([]);
+    console.log(names)
+
+
+    useEffect(() => {
+        console.log('useEffect effect callback has been called');
+        fetch("https://scratched-juniper-salto.glitch.me/kekambas")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const classMates = data;
+            console.log(classMates)
+            setNames(classMates);
+        })
+    },[]);
+
+   return (     
+        <>
+            <div className="container;">
+                <h1 class="text-center">Kekambas</h1>
+                <button type="button" class="btn btn-info mb-2">Students</button>
+                <KekambasList names={names}/>
+                <table class="table table-primary table-striped">
+                    <thead>
+                        <tr>
+                        
+                        </tr>
+                        <tbody>
+                            {names.map(names => (
+                            <tr key={names.first_name}>
+                                <th>{names.first_name}</th> 
+                                <th>{names.last_name}</th> 
+                            </tr>
+                            ))}
+                        </tbody>
+                    </thead>
+                </table>
+            
+            </div>
+        </>
+   );
+   
+  
 }
 
-export default App;
---
+++ src/components/KekambasList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function kekambasList() {
+  return (
+    <div>
+    </div>
+  )
+}
